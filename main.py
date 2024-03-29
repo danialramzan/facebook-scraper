b285fb6b55a2319f8c310d22d92828c612596c2c
Danial Ramzan <danialramzan@gmail.com>
2024-03-29 09:31:01 -0700

    reduced redundancy and moved sensitive info to .env file

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.firefox.service import Service
 # from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.firefox import GeckoDriverManager
+from dotenv import load_dotenv
+from time import sleep
+
+import os
 
 # Importing time libraries to add wait times
 
@@ -19,11 +23,10 @@ from webdriver_manager.firefox import GeckoDriverManager
 firefox_options = Options()
 # Disable notifications
 # chrome_options.add_argument("--disable-notifications")
-# git test line
-# git test line2
 
 
-profile_path = '/home/danialramzan/snap/firefox/common/.mozilla/firefox/NOOOOOOOOOOOOO'
+profile_path = os.getenv('FIREFOX_PROFILE_PATH')
+facebook_group_url = os.getenv('FACEBOOK_GROUP_URL')
 
 firefox_options.add_argument("--disable-notifications")
 
@@ -41,62 +44,46 @@ firefox_profile = FirefoxProfile()
 
 driver = webdriver.Firefox(service=service, options=firefox_options)
 # driver = webdriver.Firefox(service=GeckoDriverManager().install(), options = firefox_options)
-driver.get("https://www.facebook.com")
+driver.get(facebook_group_url)
 driver.maximize_window()
 
-# sleep(2)
-#
-# # Accept cookies
-# cookies = WebDriverWait(driver, 30).until(
-#     EC.element_to_be_clickable((By.XPATH, '//button[@class="_42ft _4jy0 _9o-t _4jy3 _4jy1 selected _51sy"]')))
-# cookies.click()
-#
-# email = driver.find_element_by_id("email")
-# email.send_keys("YOUR_EMAIL_HERE")  # replace YOUR_EMAIL_HERE with your email
-# password = driver.find_element_by_id("pass")
-# password.send_keys("YOUR_PASSWORD_HERE")  # replace YOUR_PASSWORD_HERE with your password
-# sleep(1)
-# login = driver.find_element_by_name("login")
-# login.click()
-# sleep(2)
-# driver.get("https://www.facebook.com/groups/YOUR_GROUP_HERE")  # replace YOUR_GROUP_HERE with your group
-# sleep(4)
-#
-# while True:
-#     soup = BeautifulSoup(driver.page_source, "html.parser")
-#     all_posts = soup.find_all("div", {"class": "du4w35lb k4urcfbm l9j0dhe7 sjgh65i0"})
-#     for post in all_posts:
-#         try:
-#             name = post.find("a", {
-#                 "class": "oajrlxb2 gs5ia77u qu0x051f esr5mh6w e9989ue4 r7d6kgcz rq0escxv nhd2j8a9 alzwoclg mc1re8zz eojpzvty tkr6xdv7"})
-#         except:
-#             name = "not found"
-#         print(name)
-#         try:
-#             content = post.find("span", {"class": "d2edcug0 hpfvmrgz qv66sw1b c1et5uql o1r32d6d ik7dh3pa hts83030"})
-#         except:
-#             content = "not found"
-#         print(content)
-#         try:
-#             time = post.find("a", {"class": "oajrlxb2 gs5ia77u qu0x051f esr5mh6w e9989ue4 r7d6kgcz rq0escxv nhd2j8a9"})
-#             print(time)
-#         except:
-#             time = "not found"
-#
-#         content_list.append(content)
-#         time_list.append(time)
-#         name_list.append(name)
-#
-#     df = pd.DataFrame({"name": name_list, "content": content_list, "time": time_list})
-#     df.drop_duplicates(subset="content", keep="first", inplace=True)
-#     df.to_csv("facebook_data2.csv")
-#
-#     if df.shape[0] > 10:
-#         break
-#
-#     sleep(5)
-#     y = 500
-#     for timer in range(0, 25):
-#         driver.execute_script("window.scrollTo(0, " + str(y) + ")")
-#         y += 500
-#         sleep(3)
+sleep(2)
+
+while True:
+    soup = BeautifulSoup(driver.page_source, "html.parser")
+    all_posts = soup.find_all("div", {"class": "du4w35lb k4urcfbm l9j0dhe7 sjgh65i0"})
+    for post in all_posts:
+        try:
+            name = post.find("a", {
+                "class": "oajrlxb2 gs5ia77u qu0x051f esr5mh6w e9989ue4 r7d6kgcz rq0escxv nhd2j8a9 alzwoclg mc1re8zz eojpzvty tkr6xdv7"})
+        except:
+            name = "not found"
+        print(name)
+        try:
+            content = post.find("span", {"class": "d2edcug0 hpfvmrgz qv66sw1b c1et5uql o1r32d6d ik7dh3pa hts83030"})
+        except:
+            content = "not found"
+        print(content)
+        try:
+            time = post.find("a", {"class": "oajrlxb2 gs5ia77u qu0x051f esr5mh6w e9989ue4 r7d6kgcz rq0escxv nhd2j8a9"})
+            print(time)
+        except:
+            time = "not found"
+
+        content_list.append(content)
+        time_list.append(time)
+        name_list.append(name)
+
+    df = pd.DataFrame({"name": name_list, "content": content_list, "time": time_list})
+    df.drop_duplicates(subset="content", keep="first", inplace=True)
+    df.to_csv("facebook_data2.csv")
+
+    if df.shape[0] > 10:
+        break
+
+    sleep(5)
+    y = 500
+    for timer in range(0, 25):
+        driver.execute_script("window.scrollTo(0, " + str(y) + ")")
+        y += 500
+        sleep(3)
